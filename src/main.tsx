975c7881ccd2b890e18049a145c2e3aa4839b17e
Foxxite <foxxite@foxxite.com>
2022-06-11 22:22:55 +0200

    Lazy load big components

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,19 @@
  * @format
  */
 
-import React from "react";
+import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
-import Repos from "./components/Repos/Repos";
-import Skyline from "./components/Skyline/Skyline";
 
 import "./scss/index.scss";
+
+const Repos = lazy(() => import("./components/Repos/Repos"));
+const Skyline = lazy(() => import("./components/Skyline/Skyline"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Header />
+
 		<Skyline />
 		<Repos />
 	</React.StrictMode>
